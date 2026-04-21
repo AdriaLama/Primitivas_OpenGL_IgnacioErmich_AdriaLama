49c6ca65742e31f4cbb8bdbcf21344762911bd62
NachoErmisch <nachoermisch@gmail.com>
2026-04-21 16:20:59 +0200

    cube rotation + change colors

--- MyFirstOpenGL/MyFirstFragmentShader.glsl
+++ MyFirstOpenGL/MyFirstFragmentShader.glsl
@@ -1,10 +1,20 @@
 #version 440 core
-
 uniform vec2 windowSize;
-
+uniform int objectID;
 out vec4 fragColor;
 
 void main()
 {
-    fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    if (objectID == 0)
+    {
+        if (gl_FragCoord.y > windowSize.y / 2.0)
+            fragColor = vec4(1.0, 1.0, 0.0, 1.0); // Amarillo
+        else
+            fragColor = vec4(1.0, 0.5, 0.0, 1.0); // Naranja
+    }
+    else
+    {
+        fragColor = vec4(1.0, 1.0, 1.0, 1.0); 
+    }
 }
+   
