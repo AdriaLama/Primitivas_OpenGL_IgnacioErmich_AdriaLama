d97407f4eb26816a2a4ab73d412a44d4d4fb83e7
Adria Lama <adria.lama@enti.cat>
2026-04-21 16:49:31 +0200

    ortoedro-escala/rotacion/color

--- MyFirstOpenGL/MyFirstFragmentShader.glsl
+++ MyFirstOpenGL/MyFirstFragmentShader.glsl
@@ -5,7 +5,7 @@ out vec4 fragColor;
 
 void main()
 {
-    if (objectID == 0)
+    if (objectID == 0 || objectID == 1)
     {
         if (gl_FragCoord.y > windowSize.y / 2.0)
             fragColor = vec4(1.0, 1.0, 0.0, 1.0); // Amarillo
