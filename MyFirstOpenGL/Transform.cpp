65f21895442e0a318e993d501884c58826d42672
NachoErmisch <nachoermisch@gmail.com>
2026-04-26 17:48:51 +0200

    refactorizar source

---
+++ MyFirstOpenGL/Transform.cpp
@@ -0,0 +1,19 @@
+#include "Transform.h"
+
+//Funcion que genera una matriz de traslacion representada por un vector
+glm::mat4 GenerateTranslationMatrix(glm::vec3 translation)
+{
+    return glm::translate(glm::mat4(1.0f), translation);
+}
+
+//Funcion que genera una matriz de rotacion dado un angulo y un vector
+glm::mat4 GenerateRotationMatrix(glm::vec3 axis, float fDegrees)
+{
+    return glm::rotate(glm::mat4(1.0f), glm::radians(fDegrees), glm::normalize(axis));
+}
+
+//Funcion que genera una matriz de escalado representada por un vector
+glm::mat4 GenerateScaleMatrix(glm::vec3 scaleAxis)
+{
+    return glm::scale(glm::mat4(1.0f), scaleAxis);
+}
